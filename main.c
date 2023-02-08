e69c28dc237cafe4cff388eee58543e80fe08138
Sem Valent <106388146+SemValent@users.noreply.github.com>
2023-02-08 10:33:21 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -7,19 +7,22 @@
 
 #define DEBOUNCE_PERIOD_MS 15
 
-volatile int Y_check = 0;
+volatile int Z_check = 0;
+volatile int X_cordinaat = 0;
+volatile int Y_cordinaat = 0;
 
 void init_knoppen(void){
 
  DDRF &= ~(1<<PF0);  // A0 input (button 1)
- DDRF &= ~(1<<PF1);  // A1 input (button 2)
- DDRF &= ~(1<<PF2);  // A2 input (button 3)
+ DDRF &= ~(1<<PF1);  // A1 input (joystick up)
+ DDRF &= ~(1<<PF2);  // A2 input (joystick left)
  DDRF &= ~(1<<PF3);  // A3 input (noodknop)
- DDRF &= ~(1<<PF4);  // A4 input (joystick up)
+ DDRF &= ~(1<<PF4);  // A4 input (joystick right)
  DDRF &= ~(1<<PF5);  // A5 input (joystick down)
  DDRK &= ~(1<<PK1);  // A9 input (limit switch z_as)
  DDRK &= ~(1<<PK2);  // A10 input (limit switch x_as)
-
+ DDRK &= ~(1<<PK6);  // A14 input (button 2)
+ DDRK &= ~(1<<PK7);   //A15 input (button 3)
 }
 void init_leds(void){
  DDRF |= (1<< PF7); // a7 outpout (led)
@@ -46,6 +49,9 @@ void init(void)
 void delay(int time){
     int timer = 0;
 
+    TCNT3 = 0;
+    TIFR3 = (1<<TOV3);
+
     while (timer < time){
 
             if (TIFR3 & (1<<TOV3))
@@ -57,38 +63,6 @@ void delay(int time){
 }
 
 
-int main(void)
-{
-	init();
-	 PORTF = 0b00000000;
-
-    X_as_percentage(0);
-
-    while(1){
-
-
-
-    if (button1_pressed()){
-
-		X_as_percentage(100);
-		delay(5);
-		X_as_percentage(0);
-
-     }
-
-     if (!button2_pressed()){
-        X_as_percentage(-100);
-        delay(5);
-        X_as_percentage(0);
-     }
-
-    }
-
-    return 0;
-}
-
-
-
  int button1_pressed(void)
 {
     if (PINF &(1<<PF0)){
@@ -98,7 +72,17 @@ int main(void)
         return 0;
 }
 
-int button2_pressed(void)
+ int button2_pressed(void)
+{
+    if (PINK &(1<<PK6)){
+    _delay_ms (DEBOUNCE_PERIOD_MS);
+        if (PINK &(1<<PK6)) return 1;
+        }
+        return 0;
+}
+
+
+int joystick_up(void)
 {
     if (PINF &(1<<PF1)){
     _delay_ms (DEBOUNCE_PERIOD_MS);
@@ -107,7 +91,7 @@ int button2_pressed(void)
         return 0;
 }
 
-int button3_pressed(void)
+int joystick_left(void)
 {
     if (PINF &(1<<PF2)){
     _delay_ms (DEBOUNCE_PERIOD_MS);
@@ -124,7 +108,7 @@ int nood_knop(void)
         }
         return 0;
 }
-int joystick_up(void)
+int joystick_right(void)
 {
     if (PINF &(1<<PF4)){
     _delay_ms (DEBOUNCE_PERIOD_MS);
@@ -141,7 +125,8 @@ int joystick_down(void)
         return 0;
 }
 
- int limit_switch_Zas(void)
+
+ int limit_switch_Xas(void)
 {
     if (PINK &(1<<PK1)){
     _delay_ms (DEBOUNCE_PERIOD_MS);
@@ -150,96 +135,159 @@ int joystick_down(void)
         return 0;
 }
 
- int limit_switch_Xas(void)
+ int limit_switch_Yas(void)
 {
     if (PINK &(1<<PK2)){
     _delay_ms (DEBOUNCE_PERIOD_MS);
         if (PINK &(1<<PK2)) return 1;
         }
         return 0;
+}
 
 
-//reset
-if (button1_pressed()){
 
+int button3_pressed(void)
+{
+    if (PINK &(1<<PK7)){
+    _delay_ms (DEBOUNCE_PERIOD_MS);
+        if (PINK &(1<<PK7)) return 1;
+        }
+        return 0;
+}
+
+
+void home_Xas(void)
+{
     while (limit_switch_Xas()){
-        X_as_percentage(100);
+
+		X_as_percentage(-100);
+
+     }
+
+    if (!limit_switch_Xas()){
+        X_as_percentage(0);
     }
-    X_as_percentage(0);
-
-    while(limit_switch_Zas()){
-        Z_as_percentage(100);
-    }
-    Z_as_percentage(0);
-}
-
-//Y_as
-if (button3_pressed){
-
-    if (Y_check == 1){
-        Y_as_percentage(-100);
-        delay(10);
-        Y_as_percentage(0);
-
-        PORTK = (0<<PK3);
-        delay(10);
-
-        Y_as_percentage(100);
-        delay(10);
-        Y_as_percentage(0);
-
-        Y_check = 0;
-    }
-
-    else if (Y_check == 0){
-        Y_as_percentage(-100);
-        delay(10);
-        Y_as_percentage(0);
-
-        PORTK = (1<<PK3);
-        delay(10);
-
-        Y_as_percentage(100);
-        delay(10);
-        Y_as_percentage(0);
-
-        Y_check = 1;
-    }
+    X_cordinaat = 0;
 
 }
 
-//x_as up
-if(joystick_up){
-    X_as_percentage(-100);
-    delay(10);
-    X_as_percentage(0);
+void home_Yas(void)
+{
+    while (limit_switch_Yas()){
+
+		Y_as_percentage(100);
+
+     }
+
+    if (!limit_switch_Yas()){
+        Y_as_percentage(0);
+    }
 }
 
-//x_as down
-if(joystick_down){
-    X_as_percentage(100);
-    delay(10);
-    X_as_percentage(0);
-}
+void pak_ton(void){
 
-//auto
-if(button2_pressed()){
-    //reset
-    while (!limit_switch_Xas()){
-        X_as_percentage(100);
-    }
-    X_as_percentage(0);
+    if (Z_check == 0){
 
-    while(!limit_switch_Zas()){
-        Z_as_percentage(100);
-    }
+    Z_as_percentage(-100);
+    delay(19);
     Z_as_percentage(0);
 
-    //X_as naar positie
-    //...
+    PORTK |= (1<<PK3);
+    delay(2);
+
+    Z_as_percentage(100);
+    delay(19);
+    Z_as_percentage(0);
+
+    Z_check = 1;
+    }
+
+    else if (Z_check == 1){
+
+    Z_as_percentage(-100);
+    delay(19);
+    Z_as_percentage(0);
+
+    PORTK &= ~(1<<PK3);
+    delay(2);
+
+    Z_as_percentage(100);
+    delay(19);
+    Z_as_percentage(0);
+
+    Z_check = 0;
+    }
+
 }
 
 
 
-	return 0;
+
+
+int main(void)
+{
+	init();
+
+
+    while(1){
+
+        if (!nood_knop()){
+
+
+        if (button1_pressed()){
+            home_Xas();
+            home_Yas();
+        }
+
+        if (!button2_pressed()){
+            pak_ton();
+        }
+
+        if (!button3_pressed()){
+            Z_as_percentage(100);
+        }
+
+        if (!joystick_right()){
+
+            if (X_cordinaat > 0)
+            X_as_percentage(-100);
+            delay(1);
+            X_cordinaat --;
+
+        }
+
+        if (!joystick_left()){
+
+            if (X_cordinaat < 22){
+            X_as_percentage(100);
+            delay(1);
+            X_cordinaat ++;
+            }
+        }
+
+        if (!joystick_down()){
+            Y_as_percentage(100);
+            delay(1);
+        }
+
+        if (!joystick_up()){
+            Y_as_percentage(-100);
+            delay(1);
+        }
+
+        else{
+            X_as_percentage(0);
+            Y_as_percentage(0);
+            Z_as_percentage(0);
+        }
+
+        }
+
+
+
+
+    }
+    return 0;
 }
+
+
